b94e4f6f19ae8a841593cd3235d0c97131f3c407
aelis098 <apontetbrun@gmail.com>
2025-04-17 17:31:30 +0200

    botuon

--- src/js/win.js
+++ src/js/win.js
@@ -16,8 +16,8 @@ export default class win extends Phaser.Scene {
 
     const screen_win = this.add.image(this.game.config.width/2, this.game.config.height/2, "screen_win"); // Réglez la valeur selon vos besoins
 
-    const button_back = this.add.image(this.game.config.width/2, 600, "button_back"); // Réglez la valeur selon vos besoins
-    
+    const button_back = this.add.image(this.game.config.width/1.4, 600, "button_back"); // Réglez la valeur selon vos besoins
+    const button_replay = this.add.image(this.game.config.width/3.4, 600, "button_replay"); // Réglez la valeur selon vos besoins
     
     button_back.setInteractive();    
     button_back.on("pointerover", () => {
@@ -31,6 +31,19 @@ export default class win extends Phaser.Scene {
 
     button_back.on("pointerup", () => {
       this.scene.start("accueil");
+    });
+    button_replay.setInteractive();    
+    button_replay.on("pointerover", () => {
+      button_replay.setScale(1.1);
+      button_replay.setTint(0xC0C0C0);
+    });
+    button_replay.on("pointerout", () => {
+      button_replay.setScale(1.0);
+      button_replay.clearTint();
+    });
+
+    button_replay.on("pointerup", () => {
+      this.scene.start("map_recto");
     });
    
    if (!this.sound.get('son_win')) {
